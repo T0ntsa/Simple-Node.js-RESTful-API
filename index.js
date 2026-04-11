5b0e0cc24b0e7c28b27cb3e4ccbadd4adee84301
Toni Näsman <toni.nasman@student.hamk.fi>
2026-04-11 16:50:52 +0300

    404+about

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
-
 const app = express();
 app.engine('handlebars', exphbs.engine({
     defaultLayout: 'main',
 }));
+
 app.set('view engine', 'handlebars');
 // static files
 app.use(express.static('public'));
 
-
+// HOME
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'This is title'
     });
 });
 
+// ABOUT
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About this page'
+    });
+});
+
+
+
+// Page not found 404
+app.use((req, res) => {
+    res.status(404).send(`Page ${req.host}<b>${req.originalUrl}</b> not found. <a href="/">Homepage</a>`);
+    // Log to see what was the original URL that caused the 404 error
+    console.log(`404 error: ${req.originalUrl} not found`);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
